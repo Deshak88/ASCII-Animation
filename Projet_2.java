97a7809ee45b4dd3e504bb3d909c9e88fad9f39a
Faress88 <84855682+Faress88@users.noreply.github.com>
2021-05-26 10:16:51 -0400

    Add files via upload

---
+++ Projet_2.java
@@ -0,0 +1,305 @@
+package Exam.Animations;
+///////////////////
+//
+//F4r3s5 S4d3k
+//Le 16 Février
+//Animation ASCII
+//
+///////////////////
+	
+public class Projet_2 {
+	public static void main(String args[]) throws InterruptedException {
+	
+		while(true) {	
+		
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                         	    ");
+			System.out.println("                         	    ");
+			System.out.println("                         	    ");
+			System.out.println("                         	    ");
+			System.out.println("                         	    ");
+			System.out.println("                          	    ");
+			System.out.println("                          	    ");
+		
+			Thread.sleep(100);
+		
+			//Il prend la balle sur le sol et il y a 2 spectateurs pour le reste du match
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" \\_O_/    \\_O_/     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println("            	            |   ");
+			System.out.println("   O     0 	            |       ");
+			System.out.println("  /\\    _/\\\\                |");
+			System.out.println("  |\\\\     /|                | ");
+			System.out.println(" /  |o    \\ \\	         ~~~~~~ ");
+		
+		
+			System.out.println("                        	    ");
+			System.out.println("                       	        ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+		
+			Thread.sleep(100);
+		
+			//la balle est dans ses mains
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" \\_O_/    \\_O_/     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println("                            |   ");
+			System.out.println("   O     0                  |   ");
+			System.out.println("  /\\_o  _/\\\\                |");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |     \\ \\	         ~~~~~~ ");
+		
+		
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+		
+			Thread.sleep(100);
+		
+			//Il se met en position et le bloqueur se prépare à sauter (1)
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" /_O_\\    /_O_\\     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println("                            |   ");
+			System.out.println("  O      0                  |   ");
+			System.out.println("  |\\_o  _/\\\\                |");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |     \\ \\	         ~~~~~~ ");
+		
+		
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+		
+			Thread.sleep(100);
+		
+			//Il se prépare à tirer (1) et bloqueur se prépare à sauter (2)
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" /_O_\\    /_O_\\     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println("                            |   ");
+			System.out.println("  O  o   0                  |   ");
+			System.out.println("  |\\/    _\\                 | ");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |     \\ \\	         ~~~~~~ ");
+		
+		
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+		
+			//Il se prépare à tirer (2) et le bloqueur se prépare à sauter (3)
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" \\_O_/    \\_O_/     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println("   /|     |                 |   ");
+			System.out.println("  O/      \\0                |  ");
+			System.out.println("  |        T                |   ");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |     \\ \\	         ~~~~~~ ");
+		
+		
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+		
+			Thread.sleep(100);
+			
+			//Il tire et le bloqueur saute mais manque la balle
+			System.out.println("        o     WOOO!             ");
+			System.out.println("    .,       /                  ");
+			System.out.println("   //   \\\\\\_O_/     ____|    ");
+			System.out.println("__O/_____\\0_|_______\\%/ |~~\\ ");
+			System.out.println("  T       T                 |   ");
+			System.out.println("  |       |                 |   ");
+			System.out.println("  |\\     /|                 |  ");
+			System.out.println("  ||     ||                 |   ");
+			System.out.println("  ''     ''              ~~~~~~ ");
+			
+			
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+			
+			//La balle est dans l'air
+			System.out.println("    .,  \\\\  o                 ");
+			System.out.println("   //    \\0                    ");
+			System.out.println("  O/      T_O_\\     ____|      ");
+			System.out.println("__T_______|_|_______\\%/ |~~\\  ");
+			System.out.println("  |      /|                 |   ");
+			System.out.println("  |\\     ||                 |  ");
+			System.out.println("  ||     ''                 |   ");
+			System.out.println("  ''                        |   ");
+			System.out.println("                         ~~~~~~ ");
+			
+			
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+			
+			//Le bloqueur a tombé en premier et la balle va rentrer dans le panier
+			System.out.println("    .,        WOOO!             ");
+			System.out.println("   //        /      o           ");
+			System.out.println("  O/    \\\\/_O_\\     ____|    ");
+			System.out.println("__T______\\0_|_______\\%/ |~~\\ ");
+			System.out.println("  |       T                 |   ");
+			System.out.println("  |\\      |                 |  ");
+			System.out.println("  ||     /|                 |   ");
+			System.out.println("  ''     ||                 |   ");
+			System.out.println("         ''              ~~~~~~ ");
+			
+			
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+			
+			//la balle rentre dans le panier
+			System.out.println("              WOOO!             ");
+			System.out.println("             /                  ");
+			System.out.println("   //   \\\\\\_O_/     ____|    ");
+			System.out.println("__O/_____\\0_|_______\\o\\ |~~\\");
+			System.out.println("  T       T                 |   ");
+			System.out.println("  |       |                 |   ");
+			System.out.println("  |\\     /|                 |  ");
+			System.out.println("  ||     ||                 |   ");
+			System.out.println("  ''     ''              ~~~~~~ ");
+			
+			
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+			
+			//la balle frappe le poto
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" \\_O_/    \\_O_/     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println(" { /|     |                 |   ");
+			System.out.println("  O/      \\0                |  ");
+			System.out.println("  |        T               o|   ");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |     \\ \\	         ~~~~~~ ");
+			
+			
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+			
+			//La balle roulle vers lui et le bloqueur se remet en position pour bloquer
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" /_O_\\    /_O_\\     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println(" {                          |   ");
+			System.out.println("  O      0                  |   ");
+			System.out.println("  |\\/    _\\                 | ");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |     \\ \\	   o=    ~~~~~~ ");
+			
+			
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			System.out.println("                                ");
+			
+			Thread.sleep(100);
+			
+			//Il se prépare à prendre la balle sur le sol et le bloqueur est en position pour bloquer
+			System.out.println("     YEAAAA!  WOOO!             ");
+			System.out.println("    /        /                  ");
+			System.out.println(" /_O_\\    /_O_\\     ____|     ");
+			System.out.println("___|________|_______\\%/ |~~\\  ");
+			System.out.println("                            |   ");
+			System.out.println("  O      0                  |   ");
+			System.out.println("  |\\_   _/\\\\                |");
+			System.out.println("  |\\      /|                |  ");
+			System.out.println(" /  |  o  \\ \\	         ~~~~~~ ");
+			
+		
+		}
+	}
+}
